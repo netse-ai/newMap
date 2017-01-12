2248b6d95380ed90e3ffc5767ee851afc15bb574
Trent Resh <trentresh@Trents-MBP.home>
2017-01-12 15:50:36 -0800

    forgot bootstrap dependencies

--- map.js
+++ map.js
@@ -7,7 +7,8 @@ function initMap() {
   var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
   map = new google.maps.Map(document.getElementById('map'), {
       center: pyrmont,
-      zoom: 18,
+      zoom: 19,
+      tilt: 45,
       disableDefaultUI: true,
       zoomControl: true,
     });
