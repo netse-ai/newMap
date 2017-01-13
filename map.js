7caf176c5a7bae9124eeb9aa432b496a4dc13462
Trent Resh <trentresh@Trents-MBP.home>
2017-01-12 16:16:53 -0800

    added createUserMarker. creates a user marker based on current location... contains an infoWindow

--- map.js
+++ map.js
@@ -1,9 +1,12 @@
 var map;
 var service;
 var infoWindow;
-var loc;
+var location;
+var userMarker;
+
 
 function initMap() {
+
   var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
   map = new google.maps.Map(document.getElementById('map'), {
       center: pyrmont,
@@ -15,6 +18,7 @@ function initMap() {
 
   infoWindow = new google.maps.InfoWindow();
   updateMap();
+  map.setOptions({passiveLogo: true});
 
 }
 
@@ -23,9 +27,13 @@ function initMap() {
 function updateMap(){
 
   //watchPosition is triggered each time the user moves
+  //navigator is passed a callback to handle return position
   navigator.geolocation.watchPosition(watchPositionCallback);
+
+
 }
 
+// callback for watchPosition used to handle the returned position data
 function watchPositionCallback(position){
     var pos = {
       lat: position.coords.latitude,
@@ -35,7 +43,13 @@ function watchPositionCallback(position){
 
     //intialize a request object to be sent to the nearBySearch query
     //with the currentLocation object as the specified location
-    var nearBySearchRequest = {
+    var nearByChurchSearchRequest = {
+      location: currentLocation,
+      radius: '1000',
+      types: ['church']
+    };
+
+    var nearByStoreSearchRequest = {
       location: currentLocation,
       radius: '1000',
       types: ['store']
@@ -43,14 +57,17 @@ function watchPositionCallback(position){
 
     // center the map on the currentLocation object
     map.setCenter(currentLocation);
-
-    // initialize a new PlacesService
+    createUserMarker()
+    // initialize new PlacesServices
     placesChurchService = new google.maps.places.PlacesService(map);
+    placesStoreService = new google.maps.places.PlacesService(map);
+    // servie methods used to search nearby queries based on the requests
+    placesChurchService.nearbySearch(nearByChurchSearchRequest, searchCallback);
+    placesStoreService.nearbySearch(nearByStoreSearchRequest, searchCallback);
 
-    placesChurchService.nearbySearch(nearBySearchRequest, nearbySearchCallback);
 }
 
-function nearbySearchCallback(result, status) {
+function searchCallback(result, status) {
   console.log("Outer Results: ", result);
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     console.log("Inner Results: ", result);
@@ -71,6 +88,23 @@ function createNearbySearchMarker(place) {
 
   google.maps.event.addListener(marker, 'click', function() {
     infoWindow.setContent(place.name);
+    infoWindow.open(map, this);
+  });
+}
+
+function createUserMarker(){
+  userMarker = new google.maps.Marker({
+    position: map.getCenter(),
+    icon: {
+      path: google.maps.SymbolPath.CIRCLE,
+      scale: 10
+    },
+    draggable: false,
+    map: map
+  });
+
+  google.maps.event.addListener(userMarker, 'click', function() {
+    infoWindow.setContent("<div>"+"<p>You</p>"+"</div>");
     infoWindow.open(map, this);
   });
 }
