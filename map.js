16884277e0d5c1d05a16987130604f48b96dc60c
Trent Resh <trentresh@Trents-MBP.home>
2017-01-13 15:28:57 -0800

    restructuring

--- map.js
+++ map.js
@@ -6,33 +6,57 @@ var infoWindow;
 var location;
 var userMarker;
 
-// intialize map
+//request objects
+var nearByChurchSearchRequest = {
+  location: null,
+  radius: '500',
+  types: ['church']
+};
+
+var nearByStoreSearchRequest = {
+  location: null,
+  radius: '500',
+  types: ['store']
+};
+
+
+/* INITIALIZE MAP */
+
 function initMap() {
+  //set intial starting coordinates | doesn't matter wear
   var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
   map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 20,
-      tilt: 45,
-      disableDefaultUI: true,
-      zoomControl: true,
-    });
+    center: pyrmont,
+    disableDefaultUI: true,
+    zoom: 19,
+    maxZoom: 19,
+    minZoom: 19,
+    tilt: 45,
+    draggable: false,
+  });
 
   infoWindow = new google.maps.InfoWindow();
-  updateMap();
-  map.setOptions({passiveLogo: true});
-}
-
-//updates map with current geolocation positions and query data
-function updateMap(){
-  //watchPosition is triggered each time the user moves
-  //navigator is passed a callback to handle return position
-  setInterval(function(){
-    navigator.geolocation.getCurrentPosition(watchPositionCallback);
-  }, 2000);
   createUserMarker()
+  map.setOptions({passiveLogo: true});
+
+  updateMap();
+
 }
 
-// callback for watchPosition used to handle the returned position data
+
+/* UPDATE MAP */
+
+function updateMap(){
+  //navigator is passed a callback to handle return position
+  navigator.geolocation.watchPosition(watchPositionCallback);
+  // console.log(userMarker);
+}
+
+
+/* HELPER FUNCTIONS AND CALLBACKS */
+
+// callback for watchPosition() used to handle the returned position data
+// as well as run searches
 function watchPositionCallback(position){
   var pos = {
     lat: position.coords.latitude,
@@ -40,47 +64,44 @@ function watchPositionCallback(position){
   }
   var currentLocation = new google.maps.LatLng(pos.lat, pos.lng);
 
-  //intialize a request object to be sent to the nearBySearch query
+  //update request object to be sent to the nearBySearch query
   //with the currentLocation object as the specified location
-  var nearByChurchSearchRequest = {
-    location: currentLocation,
-    radius: '1000',
-    types: ['church']
-  };
-
-  var nearByStoreSearchRequest = {
-    location: currentLocation,
-    radius: '1000',
-    types: ['store']
-  };
+  nearByChurchSearchRequest.location = currentLocation;
+  nearByStoreSearchRequest.location = currentLocation;
 
   // center the map on the currentLocation object
   map.setCenter(currentLocation);
   userMarker.setPosition(currentLocation);
+
   // initialize new PlacesServices
   placesChurchService = new google.maps.places.PlacesService(map);
   placesStoreService = new google.maps.places.PlacesService(map);
+
   // servie methods used to search nearby queries based on the requests
   placesChurchService.nearbySearch(nearByChurchSearchRequest, searchCallback);
   placesStoreService.nearbySearch(nearByStoreSearchRequest, searchCallback);
 }
 
+// callback for nearbySearch()
 function searchCallback(result, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
+  if (status == google.maps.places.PlacesServiceStatus.OK){
     for (var i = 0; i < result.length; i++) {
       var place = result[i];
       createNearbySearchMarker(place);
+      pickUpItem(place);
     }
   }
 }
 
 // creates a marker on each place object return by the nearby search
+// also adds an infoWindow to each marker
 function createNearbySearchMarker(place) {
-  var placeLoc = place.geometry.location;
   var marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location
   });
+  console.log(place.geometry.location)
+
 
   google.maps.event.addListener(marker, 'click', function() {
     infoWindow.setContent(place.name);
@@ -89,6 +110,7 @@ function createNearbySearchMarker(place) {
 }
 
 // creates a marker displayed on the user's current coordinates
+// also adds an infoWindow to the user marker
 function createUserMarker(){
   userMarker = new google.maps.Marker({
     position: map.getCenter(),
@@ -104,4 +126,8 @@ function createUserMarker(){
     infoWindow.setContent("<div>"+"<p>You</p>"+"</div>");
     infoWindow.open(map, this);
   });
+}
+
+function pickUpItem(place){
+  // console.log(place);
 }
